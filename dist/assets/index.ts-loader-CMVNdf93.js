ea0a9e3475dfada44cfdd47684d4d364dc51530e
airman5573 <byeongin@shoplic.kr>
2026-07-03 08:16:37 +0900

    Build extension bundle for viewport reset

--- dist/assets/index.ts-loader-CMVNdf93.js
+++ dist/assets/index.ts-loader-CMVNdf93.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/index.ts-DpnV-12-.js")
+      chrome.runtime.getURL("assets/index.ts-D_DXGbwt.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
